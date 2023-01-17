56b614ce7d484a55024f15db77b006fea4456add
barba <alvaro.jeria@uv.cl>
2023-01-16 21:11:50 -0300

    Added script master_doi.sql

---
+++ master_doi.sql
@@ -0,0 +1,15 @@
+create table master_doi
+(
+    doi         TEXT,
+    autores     TEXT,
+    autor_prin  TEXT,
+    autor_sec   TEXT,
+    anno        INTEGER,
+    titulo      TEXT,
+    revista     TEXT,
+    ref_revista TEXT,
+    isbn        TEXT,
+    factor      TEXT
+);
+
+
